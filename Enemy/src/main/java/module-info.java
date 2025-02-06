dbac7384de13959e879b18fafb11e7a79e64d532
Caronor <119178135+Caronor@users.noreply.github.com>
2025-02-06 14:14:03 +0100

    3

--- Enemy/src/main/java/module-info.java
+++ Enemy/src/main/java/module-info.java
@@ -2,7 +2,9 @@ import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 module Enemy {
+    requires java.desktop;
     requires Common;
+    requires jdk.compiler;
     provides IGamePluginService with dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.enemysystem.EnemyProcessor;
 }
