ea251d75bcaf6697fec98451c603ee21f48558a6
Caronor <119178135+Caronor@users.noreply.github.com>
2025-02-22 09:05:25 +0100

    AddMultipleAsteroids

--- Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
+++ Asteroids/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidPlugin.java
@@ -5,6 +5,9 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 // Class spawns asteroids in the world
@@ -13,8 +16,10 @@ public class AsteroidPlugin implements IGamePluginService {
     @Override
     //Creates a new asteroid when the game starts
     public void start(GameData gameData, World world) {
-        Entity asteroid = createAsteroid(gameData);
-        world.addEntity(asteroid);
+        ArrayList<Entity> asteroids = createAsteroid(gameData);
+        for (Entity asteroid : asteroids) {
+            world.addEntity(asteroid);
+        }
     }
 
     @Override
@@ -25,15 +30,23 @@ public class AsteroidPlugin implements IGamePluginService {
         }
     }
 
-    private Entity createAsteroid(GameData gameData) {
-        Entity asteroid = new Asteroid();
+    private ArrayList<Entity> createAsteroid(GameData gameData) {
+        ArrayList<Entity> asteroids = new ArrayList<>();
         Random rnd = new Random();
-        int size = rnd.nextInt(10) + 5;
-        asteroid.setPolygonCoordinates(size, -size, -size, -size, -size, size, size, size);
-        asteroid.setX(0);
-        asteroid.setY(0);
-        asteroid.setRadius(size);
-        asteroid.setRotation(rnd.nextInt(90));
-        return asteroid;
+
+        for (int i = 0; i < 5; i++) {
+            Entity asteroid = new Asteroid();
+
+            int size = rnd.nextInt(20) + 15; // Larger asteroids
+            asteroid.setPolygonCoordinates(size, -size, -size, -size, -size, size, size, size);
+
+            asteroid.setX(rnd.nextInt(gameData.getDisplayWidth()));
+            asteroid.setY(rnd.nextInt(gameData.getDisplayHeight()));
+            asteroid.setRadius(size);
+            asteroid.setRotation(rnd.nextInt(360));
+
+            asteroids.add(asteroid);
+        }
+        return asteroids;
     }
 }
